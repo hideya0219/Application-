a316177ec612449a6f18722693363d0d5e92290b
hideya0219 <119572041+hideya0219@users.noreply.github.com>
2022-12-12 17:20:20 +0000

    [add]indexレイアウト

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'lists/index'
+  post 'lists' => 'lists#create'
+  get 'lists/show'
+  get 'lists/edit'
+  get '/top' => 'homes#top'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
