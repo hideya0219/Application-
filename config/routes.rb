ef249ec024a00d844c915ea944715aa2f4da2167
hideya0219 <119572041+hideya0219@users.noreply.github.com>
2022-12-12 19:32:04 +0000

    [add]indexページの投稿機能作成途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'lists/index'
-  post 'lists' => 'lists#create'
-  get 'lists/show'
-  get 'lists/edit'
+  get 'books' => 'books#index'
+  post 'books' => 'books#create'
+  get 'books/show'
+  get 'books/edit'
   get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_12_165409) do
+ActiveRecord::Schema.define(version: 2022_12_12_174358) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
